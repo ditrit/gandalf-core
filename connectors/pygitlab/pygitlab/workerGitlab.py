4185b1d96c8d569960ffe033c8b121ca1df69e24
florepartout <flore.partout@imt-atlantique.net>
2020-07-15 16:10:24 +0200

    import management

---
+++ connectors/pygitlab/pygitlab/workerGitlab.py
@@ -0,0 +1,46 @@
+#from /workers import worker1,worker2 etc
+
+#from ...libraries.pyclient import ClientGandalf
+#from .workers import WorkerProject
+from pygitlab.workers.workerProject import WorkerProject
+# Import de la classe worker de base pour python
+from ..python import Worker
+from .client.clientGitlab import ClientGitlab
+
+import json
+
+
+class WorkerGitlab(Worker):
+    def Execute(self, version):
+        '''
+        configuration = { 
+            "token" : None,
+             }
+        '''
+        configFile = open('./config.json')
+        configuration = json.load(configFile)
+        
+        clientGitlab= ClientGitlab(configuration.token, configuration.url)
+        if not clientGitlab.isValidClient() :
+            raise ValueError('Invalid client')
+        
+        
+        
+
+        # Thread creation
+        workerProject = WorkerProject(self.clientGitlab, self.clientGandalf, self.version)
+        workerProject.start()
+
+        configFile.close()
+        pass
+
+
+#MAIN
+commands = ["COMMAND_1","COMMAND_2"]
+version = 2
+
+workerGitlab = WorkerGitlab(version, commands)
+workerGitlab.Run()
+
+
+
