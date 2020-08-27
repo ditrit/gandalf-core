262dc2220a4963fd4c99c0c623e05c247c0cefa3
Kévin Le Torc'h <kev29lt@gmail.com>
2020-08-27 13:03:10 +0200

    Revert "e"
    This reverts commit 77bb96f55e521e92f288a3a54b282486450fd245.

--- connectors/pyaws/pyaws/AWS/IAM/IAM.py
+++ connectors/pyaws/pyaws/AWS/IAM/IAM.py
@@ -207,15 +207,27 @@ class IAM(Client):
 
         return None
 
-    def deleteUser(self, name: str):
+    def updateGroup(self, groupName: str, newGroupName: str, newPath: str = None):
         try:
+            if newPath == None:
+                response = self.client.update_group(
+                    GroupName=groupName, NewGroupName=newGroupName)
+            else:
+                response = self.client.update_group(
+                    GroupName=groupName, NewGroupName=newGroupName, NewPath=newPath)
 
-            response = self.client.delete_user(UserName=name)
             return response
-
         except ClientError as err:
             raise err
 
         return None
 
+    def deleteGroup(self, groupName: str):
+        try:
+            response = self.client.delete_group(GroupName=groupName)
 
+            return response
+        except ClientError as err:
+            raise err
+
+        return None
