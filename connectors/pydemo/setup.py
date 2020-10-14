d7416093a2e8a740f167d1fa10b827a47a453101
Kévin Le Torc'h <kev29lt@gmail.com>
2020-10-14 11:40:36 +0200

    Pydemo setup update

--- connectors/pydemo/setup.py
+++ connectors/pydemo/setup.py
@@ -1,3 +1,16 @@
 from setuptools import setup, find_packages
+from os.path import dirname, join
 
-setup(name='pydemo', version='0.0.1', packages=find_packages())
+setup(
+    name='pydemo',
+    version='0.0.1',
+    description='Python demo connector for Gandalf',
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'gandalf_pydemo=pydemo.WorkerDemo:main'
+        ]
+    }
+)
