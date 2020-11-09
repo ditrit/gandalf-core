d02ce005358eb818354a4fca561eb625d566679c
Chen Bevin Co <bevin@chenco-server.home>
2020-11-09 10:32:00 +0000

    Pydmo import fix

--- connectors/pydemo/setup.py
+++ connectors/pydemo/setup.py
@@ -7,5 +7,9 @@ setup(
     description='Python demo connector for Gandalf',
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     long_description_content_type="text/markdown",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires = [
+        'pyworker@git+https://github.com/ditrit/gandalf.git@pyaws-connector#egg=pyworker&subdirectory=connectors/py',
+        'pyclient@git+https://github.com/ditrit/gandalf.git@pyaws-connector#egg=pyclient&subdirectory=libraries/pyclient'
+    ]
 )
