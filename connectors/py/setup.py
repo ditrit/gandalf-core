faa1523771ada99721eb3d36f7a0c7bcd0f99f05
BlivionIaG <kev29lt@gmail.com>
2020-11-09 10:13:42 +0100

    Depencies import fix

--- connectors/py/setup.py
+++ connectors/py/setup.py
@@ -1,3 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(name='pyworker', version='0.1', packages=find_packages())
+setup(
+    name='pyworker',
+    version='0.1',
+    description = 'Base worker for Python gandalf connectors',
+    packages=find_packages(),
+    install_requires = [
+        'pyclient'
+    ],
+    dependency_links = [
+        '../../libraries/pyclient'
+    ]
+)
--- libraries/pyclient/setup.py
+++ libraries/pyclient/setup.py
@@ -1,3 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(name='pyclient', version='0.1', packages=find_packages())
+setup(
+    name='pyclient',
+    version='0.1',
+    description='Python Grpc client librairy for Gandalf',
+    packages=find_packages(),
+    install_requires = [
+        'grpcio',
+        'grpcio-tools',
+        'protobuf',
+        'six'
+    ]
+)
