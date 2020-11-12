435d2a5f5e117b801ab819f1430d8077425dd40c
HerrCraziDev <herrcrazi@gmail.com>
2020-11-12 17:05:22 +0100

    WorkerState

---
+++ connectors/py/pyworker/models/WorkerState.py
@@ -0,0 +1,23 @@
+
+from threading import Lock
+
+class State:
+    ONGOING = 0
+    STOPPING = 1
+
+class WorkerState(Lock):
+    state: State = State.STOPPING
+
+    def getState(self) -> State:
+        """
+        Return the state
+        """
+        return self.state
+    
+    def setOngoingWorkerState(self):
+        with self:
+            self.state = State.ONGOING
+
+    def setStoppingWorkerState(self):
+        with self:
+            self.state = State.STOPPING
