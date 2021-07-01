7acf8e4aeaf5d098cfc8252c9c58e335d31f81b3
Orness <orness@pop-os.localdomain>
2021-07-01 10:07:49 +0200

    Continued Domain && User in WIP

--- core/test/cli/cli_test.go
+++ core/test/cli/cli_test.go
@@ -14,20 +14,80 @@ func TestCreateDomain(t *testing.T) {
 	)
 	cliClient := cli.NewClient("http://localhost:9203")
 
+	t.Log("DOMAIN.TEST >> SUCCESS")
 	name := "test"
-	parentName := ""
+	parentName := "testDomain"
 
 	// Good Scheme
 	domain := models.Domain{Name: name}
 	err := cliClient.DomainService.Create(token, domain, parentName)
 	t.Log(err)
 
-	parentName = "unknown"
-
-	// Wrong Parent
+	t.Log("DOMAIN.TEST >> FAIL - DOMAIN NAME ALREADY EXISTS")
 	domain = models.Domain{Name: name}
 	err = cliClient.DomainService.Create(token, domain, parentName)
 	t.Log(err)
+
+	t.Log("DOMAIN.TEST >> FAIL - WRONG PARENT NAME")
+	// Wrong Parent
+	parentName = ""
+
+	domain = models.Domain{Name: name}
+	err = cliClient.DomainService.Create(token, domain, parentName)
+	t.Log(err)
+
+	t.Log("DOMAIN.TEST >> FAIL - SAME PARENT NAME THAN DOMAIN NAME")
+	parentName = "test"
+
+	domain = models.Domain{Name: name}
+	err = cliClient.DomainService.Create(token, domain, parentName)
+	t.Log(err)
+}
+func TestUpdateDomain(t *testing.T) {
+	const (
+		token string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOjY2NDA1MDg1MTM4OTQ0MDAwMSwiTmFtZSI6IkFkbWluaXN0cmF0b3IyIiwiRW1haWwiOiJBZG1pbmlzdHJhdG9yMiIsIlRlbmFudCI6IiIsImV4cCI6MTYyODcyMjk3Mn0.6KTRZr9xl6rUqToWv_SUZypOVmwdRM4_sJhjRiEDpMU"
+	)
+	cliClient := cli.NewClient("http://localhost:9203")
+
+	name := "test"
+	newName := "other"
+
+	t.Log("DOMAIN.TEST >> SUCCESS")
+
+	oldDomain, err := cliClient.DomainService.ReadByName(token, name)
+	if err == nil {
+		domain := models.Domain{Name: newName}
+		err = cliClient.DomainService.Update(token, int(oldDomain.ID), domain)
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println(err)
+	}
+
+	t.Log("DOMAIN.TEST >> FAILED - Same Domain Name")
+	newName = "test"
+	if err == nil {
+		domain := models.Domain{Name: newName}
+		err = cliClient.DomainService.Update(token, int(oldDomain.ID), domain)
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println(err)
+	}
+
+	t.Log("DOMAIN.TEST >> FAILED - Do not exists")
+	name = "neo"
+	if err == nil {
+		domain := models.Domain{Name: newName}
+		err = cliClient.DomainService.Update(token, int(oldDomain.ID), domain)
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println(err)
+	}
 }
 
 func TestCreateResourceType__Pivot(t *testing.T) {
@@ -995,5 +1055,66 @@ func TestDeleteRole(t *testing.T) {
 		}
 	} else {
 		t.Log(err)
+	}
+}
+func TestCreateUser(t *testing.T) {
+	const (
+		token string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOjY2NDA1MDg1MTM4OTQ0MDAwMSwiTmFtZSI6IkFkbWluaXN0cmF0b3IyIiwiRW1haWwiOiJBZG1pbmlzdHJhdG9yMiIsIlRlbmFudCI6IiIsImV4cCI6MTYyODcyMjk3Mn0.6KTRZr9xl6rUqToWv_SUZypOVmwdRM4_sJhjRiEDpMU"
+	)
+	cliClient := cli.NewClient("http://localhost:9203")
+
+	name := "newUser"
+	email := "machin@truc.com"
+	password := "password"
+
+	t.Log("USER >> TEST >> CREATE - SUCCESS")
+
+	user := models.NewUser(name, email, password)
+	err := cliClient.UserService.Create(token, user)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func TestDeleteUser(t *testing.T) {
+	const (
+		token string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySUQiOjY2NDA1MDg1MTM4OTQ0MDAwMSwiTmFtZSI6IkFkbWluaXN0cmF0b3IyIiwiRW1haWwiOiJBZG1pbmlzdHJhdG9yMiIsIlRlbmFudCI6IiIsImV4cCI6MTYyODcyMjk3Mn0.6KTRZr9xl6rUqToWv_SUZypOVmwdRM4_sJhjRiEDpMU"
+	)
+	cliClient := cli.NewClient("http://localhost:9203")
+
+	t.Log("USER >> TEST >> DELETE - FAIL: DO NOT EXISTS")
+	name := "UserDoNotExists"
+	oldUser, err := cliClient.UserService.ReadByName(token, name)
+	if err == nil {
+		err = cliClient.UserService.Delete(token, int(oldUser.ID))
+		if err != nil {
+			t.Log((err))
+		}
+	} else {
+		t.Log((err))
+	}
+	name = "newUser"
+	t.Log("USER >> TEST >> DELETE - SUCCESS")
+
+	oldUser, err = cliClient.UserService.ReadByName(token, name)
+	if err == nil {
+		err = cliClient.UserService.Delete(token, int(oldUser.ID))
+		if err != nil {
+			t.Log((err))
+		}
+	} else {
+		t.Log((err))
+	}
+
+	t.Log("USER >> TEST >> DELETE - FAIL: ALREADY DELETED")
+
+	oldUser, err = cliClient.UserService.ReadByName(token, name)
+	if err == nil {
+		err = cliClient.UserService.Delete(token, int(oldUser.ID))
+		if err != nil {
+			t.Log((err))
+		}
+	} else {
+		t.Log((err))
 	}
 }
